2ba84ba5f6faaa5dc22133d0794d687af9f81979
random <random@frauzufall.de>
2015-02-26 17:18:38 +0100

    basic example

---
+++ example-basic/src/ofApp.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxGui.h"
+#include "ofxSortableList.h"
+
+class ofApp : public ofBaseApp{
+	
+public:
+	void setup();
+	void update();
+	void draw();
+	
+	void exit();
+
+	void keyPressed(int key);
+	void keyReleased(int key);
+    void mouseMoved(ofMouseEventArgs &args);
+    void mouseDragged(ofMouseEventArgs &args);
+    void mousePressed(ofMouseEventArgs &args);
+    void mouseReleased(ofMouseEventArgs &args);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+
+private:
+
+    void removed(RemovedElementData& data);
+    void moved(MovingElementData& data);
+
+    ofxSortableList sortable_labels;
+    ofxSortableList sortable_toggles;
+
+    string status;
+
+};
+
